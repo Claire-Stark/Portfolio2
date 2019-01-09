2a0936cfa3a8ae963e7d8111a7b3a7545ca227b7
Claire Stark <claireu@ymail.com>
2019-01-09 21:22:13 +1300

    worked out how to change the hide/show of the home page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(function(){
+	$('#home_page').hide();
+
 	$(".typed").typed({
 		strings: ["Hi, I'm Claire", "Welcome to my portfolio website.", "Let's check it out!"],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
@@ -24,7 +26,10 @@ $(function(){
 		// either html or text
 		contentType: 'html',
 		// call when done callback function
-		callback: function() {},
+		callback: function() {
+			$('#loading_page').delay(3500).fadeOut(1000);
+			$('#home_page').show(5000).fadeIn(3000);
+		},
 		// starting callback function before each string
 		preStringTyped: function() {},
 		//callback for every typed string
@@ -33,18 +38,5 @@ $(function(){
 		resetCallback: function() {}
 	});
 
-	// Loading page animation
-	// // Effect of loading 
-	// var loading = anime({
-	// 	targets: '#loading-anime .small-circle',
-	// 	opacity: function(el, i, l) { let d = 0.25; return 1 - d*i; },
-	// 	translateX: [150,0],
-	// 	easing: [.91,-0.54,.29,1.56],
-	// 	loop: 3,
-	// 	duration: 1500,
-	// 	delay: function(el, i, l) { return i * 200; }
-	// });
-	// Hides the loading page
-	$('section#loading_page.section.section-0').delay(4800).fadeOut(1000);
 
 });
